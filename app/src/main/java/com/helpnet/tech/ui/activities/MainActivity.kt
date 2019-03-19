92d624d24307e7a1fc03077635c6da675407b7ba
Santos, Uilton (Developer) <uilton.santos@sky.uk>
2019-03-19 21:54:19 +0000

    Structure for using Fragment

--- app/src/main/java/com/helpnet/tech/ui/activities/MainActivity.kt
+++ app/src/main/java/com/helpnet/tech/ui/activities/MainActivity.kt
@@ -3,44 +3,26 @@ package com.helpnet.tech.ui.activities
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.helpnet.tech.R
-import com.helpnet.tech.data.model.OS
-import com.helpnet.tech.data.network.RequestController
+import com.helpnet.tech.ui.fragments.HomeFragment
+import com.helpnet.tech.ui.fragments.InProgressFragment
+import com.helpnet.tech.ui.fragments.ProfileFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        supportActionBar?.setHomeAsUpIndicator(null)
 
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        setupActionBar(navController)
-        setupBottomNavMenu(navController)
-        //bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
-
-    private fun setupBottomNavMenu(navController: NavController) {
-        bottom_navigation?.let {
-            NavigationUI.setupWithNavController(it, navController)
-        }
-    }
-
-    private fun setupActionBar(navController: NavController) {
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -49,38 +31,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        val navigated = NavigationUI.onNavDestinationSelected(item!!, navController)
-        return navigated || super.onOptionsItemSelected(item)
+        //TODO Missing handle notification icon
+        return super.onOptionsItemSelected(item)
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                RequestController.listOSBySituation(3, 1, object : Callback<List<OS>> {
-                    override fun onFailure(call: Call<List<OS>>?, t: Throwable?) {
-                        Toast.makeText(this@MainActivity, "Shitt", Toast.LENGTH_LONG).show()
-                    }
-
-                    override fun onResponse(call: Call<List<OS>>?, response: Response<List<OS>>?) {
-                        if (response?.isSuccessful!! && response.body() != null) {
-                            //message.text = response.body().toString()
-                        }
-                    }
-
-                })
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_wip -> {
-                //message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_profile -> {
-                //message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+        val fragmentSelected: Fragment? = when (item.itemId) {
+            R.id.navigation_home -> HomeFragment()
+            R.id.navigation_wip -> InProgressFragment()
+            R.id.navigation_profile -> ProfileFragment()
+            else -> null
         }
-        false
+
+        fragmentSelected?.also {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, it).commit()
+        }
+
+        true
     }
 }
