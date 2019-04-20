78adeb1eedb8feed66afd9093fc33d9b5f631950
Santos, Uilton (Developer) <uilton.santos@sky.uk>
2019-04-20 23:28:18 +0100

    - Logout call

--- app/src/main/java/com/helpnet/tech/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/helpnet/tech/ui/fragments/ProfileFragment.kt
@@ -1,82 +1,52 @@
 package com.helpnet.tech.ui.fragments
 
-import android.net.Uri
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.helpnet.tech.R
+import com.helpnet.tech.ui.activities.LoginActivity
+import com.helpnet.tech.util.AlertDialogUtil
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [ProfileFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_profile, container, false)
+        initViews(rootView)
+        return rootView
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+    private fun initViews(rootView: View) {
+        rootView.findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            logout()
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun logout() {
+        AlertDialogUtil.showMessageOKCancel(context,
+            getString(R.string.logout),
+            getString(R.string.logout_message),
+            okListener = DialogInterface.OnClickListener { _, _ ->
+                doLogout()
+            })
+    }
+
+    private fun doLogout() {
+        FirebaseAuth.getInstance().signOut()
+
+        Intent(activity, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(this)
+            activity!!.finish()
+        }
     }
 }
