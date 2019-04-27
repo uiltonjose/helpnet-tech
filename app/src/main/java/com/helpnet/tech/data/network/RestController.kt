91e636b6a421135fb830a3c0eeb84a0cf8defe38
Santos, Uilton (Developer) <uilton.santos@sky.uk>
2019-04-27 13:51:37 +0100

    organize RestController and using cache from Retrofit.

--- app/src/main/java/com/helpnet/tech/data/network/RestController.kt
+++ app/src/main/java/com/helpnet/tech/data/network/RestController.kt
@@ -1,7 +1,10 @@
 package com.helpnet.tech.data.network
 
+import android.annotation.SuppressLint
 import com.helpnet.tech.HelpNetApplication
+import com.helpnet.tech.util.AndroidUtil
 import com.helpnet.tech.util.SharedPreferenceUtil
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -9,8 +12,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+private const val REQUEST_AGE_TIMELAPSE = 10 // 10 seconds
+private const val REQUEST_AGE_FROM_CACHE = 60 * 60 * 24 * 5 // 5 days of cache
+
 abstract class RestController {
     companion object {
+        @SuppressLint("StaticFieldLeak")
+        private val context = HelpNetApplication.application?.applicationContext!!
+        private const val cacheSize = (5 * 1024 * 1024).toLong()
+        private val myCache = Cache(context.cacheDir, cacheSize)
 
         fun create(): EndpointInterface {
             val retrofit = Retrofit.Builder()
@@ -23,26 +33,45 @@ abstract class RestController {
 
         private val client: OkHttpClient
             get() {
-
-                val logging = HttpLoggingInterceptor()
-                logging.level = HttpLoggingInterceptor.Level.BODY
-
-                val authHeader = Interceptor { chain ->
-
-                    val request = chain.request()
-                        .newBuilder()
-                        .addHeader("Authorization", SharedPreferenceUtil.getAccessToken(HelpNetApplication.application?.applicationContext!!))
-                        .build()
-
-                    return@Interceptor chain.proceed(request)
-                }
-
                 return OkHttpClient.Builder()
+                    .cache(myCache)
                     .readTimeout(45, TimeUnit.SECONDS)
                     .connectTimeout(45, TimeUnit.SECONDS)
-                    .addInterceptor(logging)
-                    .addInterceptor(authHeader)
+                    .addInterceptor(authorizationHeaderInterceptor())
+                    .addInterceptor(loggingInterceptor())
+                    .addInterceptor(checkConnectivityOrRequestFromCacheInterceptor())
                     .build()
             }
+
+        private fun authorizationHeaderInterceptor(): Interceptor {
+            return Interceptor { chain ->
+                val request = chain.request()
+                    .newBuilder()
+                    .addHeader("Authorization", SharedPreferenceUtil.getAccessToken(context))
+                    .build()
+                return@Interceptor chain.proceed(request)
+            }
+        }
+
+        private fun loggingInterceptor(): HttpLoggingInterceptor {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            return logging
+        }
+
+        private fun checkConnectivityOrRequestFromCacheInterceptor(): Interceptor {
+            return Interceptor { chain ->
+                var request = chain.request()
+                request = if (AndroidUtil.hasConnectivity(context)) {
+                    request.newBuilder().header("Cache-Control", "public, max-age=$REQUEST_AGE_TIMELAPSE").build()
+                } else {
+                    request.newBuilder().header(
+                        "Cache-Control",
+                        "public, only-if-cached, max-stale=$REQUEST_AGE_FROM_CACHE"
+                    ).build()
+                }
+                return@Interceptor chain.proceed(request)
+            }
+        }
     }
 }
